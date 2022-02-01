766f31748529676505e72d77f88a5245551412eb
iLynette <lynetteacholah@icloud.com>
2022-02-01 12:35:44 +0300

    Add images and javascript file

---
+++ script.js
@@ -0,0 +1,37 @@
+const speakerList = [{
+    id: 'speaker1',
+    name: 'Lynn Jones',
+    position: 'CO Founder and CEO sheCodes',
+    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+},
+{
+    id: 'speaker2',
+    name: 'Angie Finn',
+    position: 'CO Founder and CEO sheCodes',
+    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+},
+{
+    id: 'speaker3',
+    name: 'Macy Jones',
+    position: 'CO Founder and CEO sheCodes',
+    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+},
+{
+    id: 'speaker4',
+    name: 'Rose Jones',
+    position: 'CO Founder and CEO sheCodes',
+    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+},
+{
+    id: 'speaker5',
+    name: 'Jess Jones',
+    position: 'CO Founder and CEO sheCodes',
+    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+},
+{
+    id: 'speaker6',
+    name: 'Kaylie Jones',
+    position: 'CO Founder and CEO sheCodes',
+    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+},
+];
