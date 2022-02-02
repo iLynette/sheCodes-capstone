dea6c74a61d43558557717b951bd9876b2786603
iLynette <lynetteacholah@icloud.com>
2022-02-02 18:40:45 +0300

    Add about page, and hamburger icon

--- script.js
+++ script.js
@@ -1,37 +1,90 @@
-const speakerList = [{
-    id: 'speaker1',
-    name: 'Lynn Jones',
-    position: 'CO Founder and CEO sheCodes',
-    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+const speakers = [{
+  id: 'speaker1',
+  image: './images/speaker1.png',
+  name: 'Lynn Jones',
+  position: 'CO Founder and CEO sheCodes',
+  description: 'Lynn is the co founder of she codes.',
 },
 {
-    id: 'speaker2',
-    name: 'Angie Finn',
-    position: 'CO Founder and CEO sheCodes',
-    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+  id: 'speaker2',
+  image: './images/speaker2.png',
+  name: 'Angie Finn',
+  position: 'CO Founder and CEO sheCodes',
+  description: 'Angie is the co founder of she codes.',
 },
 {
-    id: 'speaker3',
-    name: 'Macy Jones',
-    position: 'CO Founder and CEO sheCodes',
-    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+  id: 'speaker3',
+  image: './images/speaker3.png',
+  name: 'Macy Jones',
+  position: 'CO Founder and CEO sheCodes',
+  description: 'Macy is the co founder of she codes.',
 },
 {
-    id: 'speaker4',
-    name: 'Rose Jones',
-    position: 'CO Founder and CEO sheCodes',
-    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+  id: 'speaker4',
+  image: './images/speaker4.png',
+  name: 'Rose Jones',
+  position: 'CO Founder and CEO sheCodes',
+  description: 'Rose is the co founder of she codes.',
 },
 {
-    id: 'speaker5',
-    name: 'Jess Jones',
-    position: 'CO Founder and CEO sheCodes',
-    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+  id: 'speaker5',
+  image: './images/speaker5.png',
+  name: 'Jess Jones',
+  position: 'CO Founder and CEO sheCodes',
+  description: 'Jess is the co founder of she codes.',
 },
 {
-    id: 'speaker6',
-    name: 'Kaylie Jones',
-    position: 'CO Founder and CEO sheCodes',
-    description: 'Angie is the co founder of she codes. Her journey into tech started when she was 16'
+  id: 'speaker6',
+  image: './images/speaker6.png',
+  name: 'Kaylie Jones',
+  position: 'CO Founder and CEO sheCodes',
+  description: 'Kaylie is the co founder of she codes. ',
 },
 ];
+
+const speakerSection = document.getElementById('speakers');
+
+function info() {
+  speakers.forEach((speaker) => {
+    speakerSection.innerHTML += `
+        <div class="start">
+            <div class="first">
+                <img src="${speaker.image}" alt="${speaker.name}" class="speakerImg">
+            </div>
+            <div class="speakerDescription">
+            <h3>${speaker.name}</h3>
+            <h4>${speaker.position}</h4>
+            <hr class="speakerLine">
+            <p class="description">
+                <i><a href="#">${speaker.description}</a></i>
+            </p>
+            </div>
+        </div>`;
+  });
+}
+
+window.onload = info();
+
+const menu = document.querySelector('#menu');
+const mobileNav = document.querySelector('.mobileNav');
+const menuList = document.querySelectorAll('#nav li a');
+
+function toggleMenu() {
+  if (mobileNav.style.display === 'none') {
+    mobileNav.style.display = 'block';
+    menu.src = 'images/x.png';
+  } else {
+    mobileNav.style.display = 'none';
+    menu.src = 'far-bars';
+  }
+}
+menu.addEventListener('click', toggleMenu);
+
+function hideMenu() {
+  mobileNav.style.display = 'none';
+  menu.src = 'far-bars';
+}
+
+menuList.forEach((event) => {
+  event.addEventListener('click', hideMenu);
+});
